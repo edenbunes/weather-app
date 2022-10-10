19203f094aff7606437843aba063c4b30d7a18c2
edenbunes <115397221+edenbunes@users.noreply.github.com>
2022-10-10 10:09:08 +0300

    added code

---
+++ index.js
@@ -0,0 +1,76 @@
+let time = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[time.getDay()];
+function getZero() {
+  let minutsPlusZero = time.getMinutes();
+  if (minutsPlusZero < 10) {
+    return `0` + minutsPlusZero;
+  } else {
+    return minutsPlusZero;
+  }
+}
+let minutes = getZero();
+function getZeroForHours() {
+  let HoursPlusZero = time.getHours();
+  if (HoursPlusZero < 10) {
+    return `0` + HoursPlusZero;
+  } else {
+    return HoursPlusZero;
+  }
+}
+let hours = getZeroForHours();
+let dayAndHour = document.querySelector("#day-hour");
+dayAndHour.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showCityTemp(response) {
+  document.querySelector("#card-title").innerHTML = response.data.name;
+  document.querySelector("#degrees-number").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function activateTheCity(city) {
+  let apiKey = "33fd04d85cdb641fd1bc55ca0162ba48";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(apiUrl).then(showCityTemp);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  activateTheCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", changeCity);
+function activateCurrentButton(event) {
+  event.preventDefault();
+  function getPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = "33fd04d85cdb641fd1bc55ca0162ba48";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&&units=metric`;
+    axios.get(apiUrl).then(showCityTemp);
+  }
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+let buttunCurrentTemp = document.querySelector("#current");
+buttunCurrentTemp.addEventListener("click", activateCurrentButton);
+
+activateTheCity("tel aviv");
